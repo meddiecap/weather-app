1d5494e69b9ab154422c5970a28b4f98a564d0bb
meddiecap <postvoormathijs@gmail.com>
2025-09-02 19:50:59 -0700

    Create Card.vue

---
+++ app/components/ui/Card.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card bg-base-100 card-md shadow-sm">
+        <div class="card-body">
+            <h2 class="card-title">{{ title }}</h2>
+            <slot name="content"></slot>
+            <slot class="mt-6" name="footer"></slot>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps<{
+    title?: string
+}>()
+</script>
