d529215f8bd7cdb2b999a4281027f52eb876b5a0
meddiecap <postvoormathijs@gmail.com>
2025-09-02 21:35:46 -0700

    Update Card.vue
    optional title fix

--- app/components/ui/Card.vue
+++ app/components/ui/Card.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="card bg-base-100 card-md shadow-sm">
         <div class="card-body">
-            <h2 class="card-title">{{ title }}</h2>
+            <h2 v-if="title" class="card-title">{{ title }}</h2>
             <slot name="content"></slot>
             <slot class="mt-6" name="footer"></slot>
         </div>
