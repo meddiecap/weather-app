561eb9e72eee330e447a0605f8199aaecd81d2d1
meddiecap <postvoormathijs@gmail.com>
2025-09-08 13:12:12 -0700

    Update Card.vue
    unused css class removed

--- app/components/ui/Card.vue
+++ app/components/ui/Card.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card bg-base-100 card-md shadow-sm">
+    <div class="card bg-base-100 shadow-sm">
         <div class="card-body">
             <h2 v-if="title" class="card-title">{{ title }}</h2>
             <slot name="content" />
